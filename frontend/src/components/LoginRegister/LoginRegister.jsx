351a3c5fd748adc7f54c8464d52722f31cee8ded
plinscho <paul.linschoten@gmail.com>
2025-05-08 15:00:18 +0200

    LoginSignup appearence done, needs conection to endpoints

--- frontend/src/components/LoginRegister/LoginRegister.jsx
+++ frontend/src/components/LoginRegister/LoginRegister.jsx
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import './LoginRegister.css'
 import user_icon from '../../assets/user.png'
 import pwd_icon from '../../assets/password.png'
 
 const LoginRegister = () => {
+
+  const [action, setAction] = useState("Sign Up");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input" placeholder="Username">
+        <div className="input">
           <img src={user_icon} alt="" />
-          <input type="text" />
+          <input type="text" placeholder='Username'/>
         </div>
-        <div className="input" placeholder="Password">
+        <div className="input" >
           <img src={pwd_icon} alt="" />
-          <input type="password" />
+          <input type="password"  placeholder="Password"/>
         </div>
       </div>
-      <div className="forgot-password">Forgot password? <span>Click here</span></div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot password? <span>Click here</span></div>}
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
   )
